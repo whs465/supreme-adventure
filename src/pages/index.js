78b250ceb010fa5e3bc07620fcdb5243b127de6c
William Hernandez Sierra <whs465@gmail.com>
2018-05-29 11:40:41 +0630

    Fix header main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Bienvenido a mi waebsite</p>
+    <p>Welcome to my Website</p>
     <p>Habra francachela y habra comilona!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
